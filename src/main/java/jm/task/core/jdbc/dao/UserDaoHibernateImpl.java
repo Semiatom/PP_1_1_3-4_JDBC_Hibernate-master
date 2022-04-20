85b3bb3baabfcdd007303f0849a4aecf7b5b7cf6
Semiatom <xmingert@yandex.ru>
2022-04-20 16:12:35 +0300

    Not ready

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,16 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
+import java.sql.Connection;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    private final SessionFactory sessionFactory = new Util().getSessionFactory();
     public UserDaoHibernateImpl() {
 
     }
@@ -22,6 +28,14 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+
+        try (Session session = sessionFactory.getCurrentSession()){
+            User user = new User(name, lastName, age);
+
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        }
 
     }
 
